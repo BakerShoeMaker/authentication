f84b0961b7f4b6805f951fb69d57f735e7b2ce05
BakerShoeMaker <info@quackenworth.com>
2018-04-10 21:21:26 -0700

    finished with video 2. start on video 3.

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
-/**
- * Created by DH-Black PC on 4/10/2018.
- */
+var express                 = require('express'),
+    mongoose                = require('mongoose'),
+    passport                = require('passport'),
+    bodyParser              = require('body-parser'),
+    User                    = require('./models/user');
+    LocalStategy            = require('passport-local'),
+    passportLocalMongoose   = require('passport-local')
+
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
+
+var app = express();
+
+
+const SEVERPORT = 3000;
+
+app.set('view engine', 'ejs');
+//for express session.
+app.use(require('express-session')({
+        secret: "The dog is great",
+        resave: false,
+        saveUnitialize:false
+    }
+));
+
+//==================== USE STATEMENTS ===============
+//===================================================
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//==================== ROUTES =======================
+//===================================================
+app.get("/", function (req, res) {
+    res.render("home");
+    }
+);
+
+app.get('/main', function(req, res){
+    res.render('main');
+
+});
+
+app.listen(process.env.PORT || SEVERPORT, function () {
+    console.log("The server has started on port: " + process.env.PORT || SEVERPORT);
+});
